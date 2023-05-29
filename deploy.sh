727d1f588de15e550765ef9bd7206dcf37396bfa
Chheung <chienchheung@gmail.com>
2023-05-29 13:52:53 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ echo "====================================="
 echo "Applying k8s deployments and ingress"
 echo "=====================================\n"
 
-kubectl apply -f ./k8s/deploy-app1.yaml
-kubectl apply -f ./k8s/deploy-app2.yaml
-kubectl apply -f ./k8s/service-app1.yaml
-kubectl apply -f ./k8s/service-app2.yaml
-kubectl apply -f ./k8s/ingress.yaml
+kubectl apply -f ./k8s/deploy-app1.yaml && \
+kubectl apply -f ./k8s/deploy-app2.yaml && \
+kubectl apply -f ./k8s/service-app1.yaml && \
+kubectl apply -f ./k8s/service-app2.yaml && \
+kubectl apply -f ./k8s/ingress.yaml && \
 kubectl apply -f ./k8s/load-balancer.yaml
