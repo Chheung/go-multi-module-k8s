230328807a0e9361dbbaa5304738d66114191004
Chheung <chienchheung@gmail.com>
2023-05-29 10:00:13 +0700

    udpate

--- app2/main.go
+++ app2/main.go
@@ -3,12 +3,11 @@ package main
 import "github.com/gofiber/fiber/v2"
 
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World from app 2!")
-    })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World from app 2!")
+	})
 
-    app.Listen(":3000")
+	app.Listen(":3000")
 }
-
