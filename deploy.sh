039961efeabb9620d6d733c7ae2db673401479d8
Chheung <chienchheung@gmail.com>
2023-05-29 13:51:55 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,4 @@ kubectl apply -f ./k8s/deploy-app2.yaml
 kubectl apply -f ./k8s/service-app1.yaml
 kubectl apply -f ./k8s/service-app2.yaml
 kubectl apply -f ./k8s/ingress.yaml
+kubectl apply -f ./k8s/load-balancer.yaml
