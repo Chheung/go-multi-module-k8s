ac9524b7897b286580a4970803d6e7b5cfba60cf
Chheung <chienchheung@gmail.com>
2023-05-29 10:43:06 +0700

    udpate

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+echo "====================================="
+echo "Applying k8s deployments and ingress"
+echo "=====================================\n"
+
 kubectl apply -f ./k8s/deploy-app1.yaml
 kubectl apply -f ./k8s/deploy-app2.yaml
 kubectl apply -f ./k8s/ingress.yaml
